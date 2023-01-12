3f9217024456beb5f61c8c907265ae150afd4afb
devihoesadha <depifebrita@gmail.com>
2023-01-12 16:40:39 +0700

    Fixing delete code

--- routes/productRoute.js
+++ routes/productRoute.js
@@ -9,6 +9,6 @@ route.get("/add", Controller.addProductForm) // form untuk add product
 route.post("/add", Controller.addProductHandler) // post product yg baru ditambah dan di redirect ke (home/product)
 route.get("/detail/:id/edit", Controller.getEditProductById) // edit product
 route.post("/detail/:id/edit", Controller.postEditProduct) // post edit product
-route.get("delete/:id", Controller.deleteProduct) //delete product
+route.get("/delete/:id", Controller.deleteProduct) //delete product
 
 module.exports = route
