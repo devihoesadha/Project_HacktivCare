0061d3b4c0871a89680443d345981e508221ae9e
DimasNugrohoRamadhan <yonkav28@gmail.com>
2023-01-12 18:14:38 +0700

    updated

--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,7 @@ const route = express.Router()
 const routeUser = require('./userRoute')
 const routeProduct = require('./productRoute')
 
-// route.get('/', Controller.renderHome)
+route.get('/', Controller.renderHome)
 
 route.use('/user', routeUser)
 route.use('/products', routeProduct)
